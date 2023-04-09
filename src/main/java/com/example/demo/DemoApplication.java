5107ca324b7ab422fc5c2441a12ac60e406e1658
서민재 <ch4570@naver.com>
2023-04-09 23:18:38 +0900

    [Chore] 주석 제거

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// 주석추가
+
 @SpringBootApplication
 public class DemoApplication {
 
