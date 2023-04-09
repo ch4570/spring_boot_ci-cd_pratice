51fd56e98870239b03c5e1ca348e624a742e9d9d
서민재 <ch4570@naver.com>
2023-04-09 23:42:39 +0900

    [Chore] 주석 해제

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// 주석추가
 @SpringBootApplication
 public class DemoApplication {
 
