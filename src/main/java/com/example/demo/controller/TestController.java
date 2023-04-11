12daf6c21c32200e85f680d7e4eb95623079859e
서민재 <>
2023-04-11 13:48:48 +0900

    [Feature] TestController 수정

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,14 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //
 @RestController
 public class TestController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "자유 대한민국을 수호하는 애국보수 만세";
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name) {
+        return name.concat(" 이 요청을 시도 하였습니다.");
     }
 }
